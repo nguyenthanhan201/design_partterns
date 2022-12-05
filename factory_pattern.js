13f10d3311d443d110cc020bf1d44bf80bc35238
nguyenthanhan201 <fxannguyen201@gmail.com>
2022-12-05 09:07:55 +0700

    ci: update factory parttern

--- factory_pattern.js
+++ factory_pattern.js
@@ -1,1 +1,30 @@
-console.log("oki");
+const Developer = function (name) {
+  this.name = name;
+  this.type = "Developer";
+};
+
+const Tester = function (name) {
+  this.name = name;
+  this.type = "Tester";
+};
+
+const EmployeeFactory = function () {
+  this.create = (name, type) => {
+    switch (type) {
+      case 1:
+        return new Developer(name);
+      case 2:
+        return new Tester(name);
+    }
+  };
+};
+
+const say = () => {
+  console.log("Hi, I am " + this.name + " and I am a " + this.type);
+};
+
+const employeeFactory = new EmployeeFactory();
+const employees = [];
+employees.push(employeeFactory.create("John", 1));
+employees.push(employeeFactory.create("Jane", 2));
+console.log(employees);
